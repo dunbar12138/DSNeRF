b82b2993c930613f322e513c0a22110bfd7126b3
Andrew Liu <ahliu@google.com>
2021-03-10 14:42:00 -0500

    eval_sciprt part1

--- eval_metrics_script.py
+++ eval_metrics_script.py
@@ -1,4 +1,18 @@
+from absl import app
+from  absl import flags
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('generated_views', '', 'Directory to generated views.')
+flags.DEFINE_string('ground_truth_views', '', 'Directory to ground truth views.')
 
 def main():
   if len(argv) > 1:
     raise app.UsageError('Too many command-line arguments.')
+
+  # recursively searches generated_veiws
+
+
+
+if __name__ == '__main__':
+  app.run(main)
